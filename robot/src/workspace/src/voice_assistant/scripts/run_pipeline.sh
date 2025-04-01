9d69d0a7bbb805e117c8dd200fdb2b8dcf4fc241
ProjectRobal <patryk1-70@wp.pl>
2025-04-01 22:58:05 +0200

    ROS microphone crash pipeline but at least it is feeding pipeline with something

--- robot/src/workspace/src/voice_assistant/scripts/run_pipeline.sh
+++ robot/src/workspace/src/voice_assistant/scripts/run_pipeline.sh
@@ -4,4 +4,8 @@ install_path=$(echo "$CMAKE_PREFIX_PATH" | tr ':' '\n' | grep "voice_assistant")
 
 cd -P "$install_path"/lib/voice_assistant/rhasspy3
 
+# script/run bin/mic_record_sample.py sample.wav
+
+# script/run bin/mic_test_energy.py
+
 script/run bin/pipeline_run.py --debug
