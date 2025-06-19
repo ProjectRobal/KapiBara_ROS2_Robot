26c3d143b7c1cc2c89f910c8c01b13891c908fcf
ProjectRobal <patryk1-70@wp.pl>
2025-06-19 23:22:40 +0200

    Now simulation container checks whether nvidia or amd gpu is present

--- sim.sh
+++ sim.sh
@@ -10,20 +10,31 @@ clear
 
 xhost local:docker
 
+if lshw -C display 2>/dev/null | grep -i "vendor" | grep -qi "nvidia"; then
+    echo "NVIDIA is present as a display vendor."
+
+    VENDOR="nvidia"
+else
+
+    VENDOR="amd"
+fi
+
+
+
 arg=$1
 
 
 if [ "$arg" = "start" ]; then
 
-docker compose -f compose_sim.yml up --remove-orphans
+docker compose -f compose_sim_${VENDOR}.yml up --remove-orphans
 
 elif [ "$arg" = "start_ros" ]; then
 
-docker compose -f compose_sim.yml exec -t gazebo /run_sim.sh
+docker compose -f compose_sim_${VENDOR}.yml exec -t gazebo /run_sim.sh
 
 elif [ "$arg" = "stop" ]; then
 
-docker compose -f compose_sim.yml stop -t 3600
+docker compose -f compose_sim_${VENDOR}.yml stop -t 3600
 
 elif [ "$arg" = "clean" ]; then
 
@@ -32,55 +43,55 @@ rm -R robot/src/workspace/sim_install
 
 elif [ "$arg" = "compile" ]; then
 
-docker compose -f compose_sim.yml exec gazebo /app/cmd/build_packages.sh camera_ros
+docker compose -f compose_sim_${VENDOR}.yml exec gazebo /app/cmd/build_packages.sh camera_ros
 
 elif [ "$arg" = "purge" ]; then
 
-docker compose -f compose_sim.yml down
+docker compose -f compose_sim_${VENDOR}.yml down
 
 elif [ "$arg" = "rebuild" ]; then
 
-docker compose -f compose_sim.yml build --no-cache
+docker compose -f compose_sim_${VENDOR}.yml build --no-cache
 
 elif [ "$arg" = "build" ]; then
 
-docker compose -f compose_sim.yml build
+docker compose -f compose_sim_${VENDOR}.yml build
 
 elif [ "$arg" = "cmd" ]; then
 
-docker compose -f compose_sim.yml exec gazebo /app/cmd/run_cmd.sh "${@:2}"
+docker compose -f compose_sim_${VENDOR}.yml exec gazebo /app/cmd/run_cmd.sh "${@:2}"
 
 elif [ "$arg" = "logs" ]; then
 
-docker compose -f compose_sim.yml logs gazebo
+docker compose -f compose_sim_${VENDOR}.yml logs gazebo
 
 elif [ "$arg" = "exec" ]; then
 
-docker compose -f compose_sim.yml exec gazebo "${@:2}"
+docker compose -f compose_sim_${VENDOR}.yml exec gazebo "${@:2}"
 
 elif [ "$arg" = "run" ]; then
 
-docker compose -f compose_sim.yml run gazebo /app/cmd/run_cmd.sh "${@:2}"
+docker compose -f compose_sim_${VENDOR}.yml run gazebo /app/cmd/run_cmd.sh "${@:2}"
 
 elif [ "$arg" = "debug" ]; then
 
-docker compose -f compose_sim.yml exec gazebo bash
+docker compose -f compose_sim_${VENDOR}.yml exec gazebo bash
 
 elif [ "$arg" = "topics" ]; then
 
-docker compose -f compose_sim.yml exec gazebo /app/cmd/run_cmd.sh ros2 topic list -t
+docker compose -f compose_sim_${VENDOR}.yml exec gazebo /app/cmd/run_cmd.sh ros2 topic list -t
 
 elif [ "$arg" = "echo" ]; then
 
-docker compose -f compose_sim.yml exec gazebo /app/cmd/run_cmd.sh ros2 topic echo "${@:2}"
+docker compose -f compose_sim_${VENDOR}.yml exec gazebo /app/cmd/run_cmd.sh ros2 topic echo "${@:2}"
 
 elif [ "$arg" = "info" ]; then
 
-docker compose -f compose_sim.yml exec gazebo /app/cmd/run_cmd.sh ros2 topic info "${@:2}"
+docker compose -f compose_sim_${VENDOR}.yml exec gazebo /app/cmd/run_cmd.sh ros2 topic info "${@:2}"
 
 elif [ "$arg" = "publish" ]; then
 
-docker compose -f compose_sim.yml exec gazebo /app/cmd/run_cmd.sh ros2 topic pub --once "${@:2}"
+docker compose -f compose_sim_${VENDOR}.yml exec gazebo /app/cmd/run_cmd.sh ros2 topic pub --once "${@:2}"
 
 else
 
