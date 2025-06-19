fe814a0510cb9f418e29330c3efb15371ee4a5bc
ProjectRobal <patryk1-70@wp.pl>
2025-06-19 23:10:31 +0200

    Simulation is working great

--- robot/entrypoint.sh
+++ robot/entrypoint.sh
@@ -3,16 +3,6 @@
 
 # udevadm control --reload 
 
-sudo apt-get update
-
-rosdep update
-
-pip3 install --upgrade pip 
-
-rosdep install --from-paths /app/src/workspace/src --ignore-src  -r -y -q
-
-find /app/src/workspace/src -name "dependencies.txt" -exec pip3 install -r {} \;
-
 # # xargs apt -y install < /app/dep/packages.txt
 
 cleanup() {
--- robot/run.sh
+++ robot/run.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+sudo apt update
+
+sudo xargs apt -y install < /app/dep/packages.txt
+
+rosdep update
+
+pip3 install --upgrade pip 
+
+rosdep install --from-paths /app/src/workspace/src --ignore-src  -r -y -q
+
+find /app/src/workspace/src -name "dependencies.txt" -exec pip3 install -r {} \;
 
 
 source /opt/ros/humble/setup.bash
